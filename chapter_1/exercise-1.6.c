2d3747e7b1b345ab0ad85e9b7765dc752860eb76
ChineduEjiogu1 <chineduejiogu@gmail.com>
2024-04-27 20:03:19 -0400

    exercise 1.7 completed

---
+++ chapter_1/exercise-1.6.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+//https://stackoverflow.com/questions/17745745/exercise-1-6-kr-how-to-verfiy-that-getchar-eof-is-0-or-1
+int main()
+{
+    printf("%d\n", getchar() != EOF);
+}
